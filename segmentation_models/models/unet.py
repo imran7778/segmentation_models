7a08d72ba4f326f961b126548d9537e3b9ef5833
imran7778 <31092568+imran7778@users.noreply.github.com>
2022-09-29 13:10:10 +0200

    Update unet.py

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -22,7 +22,7 @@ def get_submodules():
         'utils': keras_utils,
     }
 
-
+# sjdgvskjfcvwhkcjsdvcskhcvshdgcvshgdcs
 # ---------------------------------------------------------------------
 #  Blocks
 # ---------------------------------------------------------------------
